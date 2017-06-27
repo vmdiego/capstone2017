fbda80d21d638f04333535b15cdbf5996699f7d8
ZachIsMe <zrp@pdx.edu>
2017-06-26 19:20:42 -0700

    added about java class
    basic layout and includes

---
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/about.java
@@ -0,0 +1,18 @@
+package com.groupc.officelocator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+/**
+ * Created by zachary on 6/26/17.
+ */
+
+public class about extends AppCompatActivity {
+    public TextView textView4;
+
+    
+
+}
