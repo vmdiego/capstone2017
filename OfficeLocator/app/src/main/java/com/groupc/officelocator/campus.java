82d0ee67103b0daeaa49d3428ff1449651e76b4c
Nhi Vu <vunhi@pdx.edu>
2017-06-30 02:46:46 -0700

    Added more buildings
    Added 2 more buildings (Tiger Woods, Nolan Ryan).
    Tried to implement button-to-page action for each building in a loop but wasn't successful. Code is commented out in hopes that I can get it right in the future.

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
@@ -1,5 +1,6 @@
 package com.groupc.officelocator;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -10,19 +11,40 @@ import android.widget.TextView;
 
 public class campus extends AppCompatActivity {
 
-    //public Button campusb2;
-
-    //6/29/2017 Nhi - Adding Mia Hamm buttons
+    //6/29/2017 Nhi - Adding buttons for the individual buildings
+    public int numberBuildings = 3; //16;
     public Button miaHamm;
     public Button mikeSchmidt;
     public Button danFouts;
+    public Button tigerWoods;
+    public Button nolanRyan1;
+
+    /*Button[] arrayButtons = {miaHamm, mikeSchmidt, danFouts};
+    int[] buttonNames = {R.id.mh1, R.id.ms1, R.id.df1};
+    String[] arrayFloorNames = {"Mia Hamm Floor Plan", "Mike Schmidt Floor Plan", "Dan Fouts Floor Plan"};
+    String[] arrayFloorPlanImages = {"miahammfloorplan", "mikeschmidtfloorplan", "danfoutsfloorplan"};
+    public int i;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
 
-        //6/29/2017 Nhi - Adding Mia Hamm buttons
+        /*for (i = 0; i < numberBuildings; ++i) {
+            arrayButtons[i] = (Button)findViewById(buttonNames[i]);
+            arrayButtons[i].setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Intent goToFloorPlan = new Intent(campus.this, floorplan.class);
+                    goToFloorPlan.putExtra("fpname", arrayFloorNames[i]);
+                    //goToFloorPlan.putExtra("imageName", arrayFloorPlanImages[i]);
+                    startActivity(goToFloorPlan);
+                }
+            });
+        }
+        */
+
+        //Mia Hamm Building
         miaHamm = (Button)findViewById(R.id.mh1);
         miaHamm.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -33,6 +55,7 @@ public class campus extends AppCompatActivity {
             }
         });
 
+        //Mike Schmidt Building
         mikeSchmidt = (Button)findViewById(R.id.ms1);
         mikeSchmidt.setOnClickListener(new View.OnClickListener(){
                                        public void onClick(View v){
@@ -44,6 +67,7 @@ public class campus extends AppCompatActivity {
                                    }
         );
 
+        //Dan Fouts Building
         danFouts = (Button)findViewById(R.id.df1);
         danFouts.setOnClickListener(new View.OnClickListener(){
                                            public void onClick(View v){
@@ -54,17 +78,34 @@ public class campus extends AppCompatActivity {
                                            }
                                        }
         );
+
+        //Tiger Woods Building
+        tigerWoods = (Button)findViewById(R.id.tw1);
+        tigerWoods.setOnClickListener(new View.OnClickListener(){
+                                        public void onClick(View v){
+                                            Intent goToFloorPlan = new Intent(campus.this, floorplan.class);
+                                            goToFloorPlan.putExtra("fpname", "Tiger Woods Floor Plan");
+                                            goToFloorPlan.putExtra("imageName", "miahammfloorplan");
+                                            startActivity(goToFloorPlan);
+                                        }
+                                    }
+        );
+
+        //Nolan Ryan Building
+        nolanRyan1 = (Button)findViewById(R.id.nr1);
+        nolanRyan1.setOnClickListener(new View.OnClickListener(){
+                                          public void onClick(View v){
+                                              Intent goToFloorPlan = new Intent(campus.this, floorplan.class);
+                                              goToFloorPlan.putExtra("fpname", "Nolan Ryan Floor Plan");
+                                              goToFloorPlan.putExtra("imageName", "danfoutsfloorplan");
+                                              startActivity(goToFloorPlan);
+                                          }
+                                      }
+        );
     }
 
 
-        /*
-        campusb2 = (Button)findViewById(R.id.campusbutton2);
-        campusb2.setOnClickListener(new View.OnClickListener(){
-                                            public void onClick(View v){
-                                                Intent theintent = new Intent(campus.this, floorplan.class);
-                                                startActivity(theintent);
-                                            }
-                                        }
-        );
-        */
+
+
+
 }
