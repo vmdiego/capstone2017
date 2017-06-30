059280a8509b2c292a8c903d645f6e489a6fdd03
Nhi Vu <vunhi@pdx.edu>
2017-06-30 01:06:03 -0700

    Added interactive map functionality
    Created base for the interactive campus map
    - Added buttons for 3 buildings in the Nike Campus map (Mia Hamm, Mike Schmidt, and Dan Fouts). Each building's button gives a different floor plan.

--- OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/campus.java
@@ -5,62 +5,66 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 //import android.widget.CheckBox;
-
 
 public class campus extends AppCompatActivity {
 
+    //public Button campusb2;
 
-
-
-    public Button campusb2;
-
-
-
-
-
-
-
-
+    //6/29/2017 Nhi - Adding Mia Hamm buttons
+    public Button miaHamm;
+    public Button mikeSchmidt;
+    public Button danFouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
 
+        //6/29/2017 Nhi - Adding Mia Hamm buttons
+        miaHamm = (Button)findViewById(R.id.mh1);
+        miaHamm.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent goToFloorPlan = new Intent(campus.this, floorplan.class);
+                goToFloorPlan.putExtra("fpname", "Mia Hamm Floor Plan");
+                goToFloorPlan.putExtra("imageName", "miahammfloorplan");
+                startActivity(goToFloorPlan);
+            }
+        });
 
-
-
-
-
-        campusb2 = (Button)findViewById(R.id.campusbutton2);
-
-        campusb2.setOnClickListener(new View.OnClickListener(){
-
-                                            public void onClick(View v){
-                                                Intent theintent = new Intent(campus.this, floorplan.class);
-
-                                                startActivity(theintent);
-                                            }
-
-                                        }
-
+        mikeSchmidt = (Button)findViewById(R.id.ms1);
+        mikeSchmidt.setOnClickListener(new View.OnClickListener(){
+                                       public void onClick(View v){
+                                           Intent goToFloorPlan = new Intent(campus.this, floorplan.class);
+                                           goToFloorPlan.putExtra("fpname", "Mike Schmidt Floor Plan");
+                                           goToFloorPlan.putExtra("imageName", "mikeschmidtfloorplan");
+                                           startActivity(goToFloorPlan);
+                                       }
+                                   }
         );
 
-
-
-
-
+        danFouts = (Button)findViewById(R.id.df1);
+        danFouts.setOnClickListener(new View.OnClickListener(){
+                                           public void onClick(View v){
+                                               Intent goToFloorPlan = new Intent(campus.this, floorplan.class);
+                                               goToFloorPlan.putExtra("fpname", "Dan Fouts Floor Plan");
+                                               goToFloorPlan.putExtra("imageName", "danfoutsfloorplan");
+                                               startActivity(goToFloorPlan);
+                                           }
+                                       }
+        );
     }
 
 
-
-
-
-
-
-
-
-
-
+        /*
+        campusb2 = (Button)findViewById(R.id.campusbutton2);
+        campusb2.setOnClickListener(new View.OnClickListener(){
+                                            public void onClick(View v){
+                                                Intent theintent = new Intent(campus.this, floorplan.class);
+                                                startActivity(theintent);
+                                            }
+                                        }
+        );
+        */
 }
--- OfficeLocator/app/src/main/java/com/groupc/officelocator/floorplan.java
+++ OfficeLocator/app/src/main/java/com/groupc/officelocator/floorplan.java
@@ -2,12 +2,14 @@ package com.groupc.officelocator;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 //import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.view.View.OnClickListener;
@@ -20,10 +22,7 @@ public class floorplan extends AppCompatActivity {
     public Button floorplanb1;
 
     public Button floorplanb2;
-
-
-
-
+    ImageView floorPlanImage;
 
 
 
@@ -34,40 +33,38 @@ public class floorplan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floorplan);
 
+        Intent goToFloorPlan = getIntent();
 
+        String fpname = goToFloorPlan.getStringExtra("fpname");
+        TextView floorplanname = (TextView) findViewById(R.id.floorPlanName);
+        floorplanname.setText(fpname);
 
-
+        String imageName = goToFloorPlan.getStringExtra("imageName");
+        int res = getResources().getIdentifier(imageName, "drawable", this.getPackageName());
+        floorPlanImage = (ImageView)findViewById(R.id.floorPlanImage);
+        floorPlanImage.setImageResource(res);
 
 
         floorplanb1 = (Button)findViewById(R.id.floorplanbutton1);
-
         floorplanb1.setOnClickListener(new View.OnClickListener(){
-
                                         public void onClick(View v){
                                             Intent theintent = new Intent(floorplan.this, campus.class);
-
                                             startActivity(theintent);
                                         }
-
                                     }
 
         );
 
 
-
-
         floorplanb2 = (Button)findViewById(R.id.floorplanbutton2);
-
         floorplanb2.setOnClickListener(new View.OnClickListener(){
 
                                            public void onClick(View v){
                                                Intent theintent = new Intent(floorplan.this, directions.class);
-
                                                startActivity(theintent);
                                            }
 
                                        }
-
         );
 
 
